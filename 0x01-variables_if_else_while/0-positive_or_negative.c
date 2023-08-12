84aeee7405179840d38b8f7b99e45a4f2db23e16
Mehdi <kah.mehdi@gmail.com>
2023-08-11 19:31:46 -0700

    fix trailing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,11 +4,11 @@
 #include <stdio.h>
 /* betty style doc for function main goes there */
 
-/*
- *main - main function
+/**
+ * main - main function
  *
- * Return -> always 0
- * */
+ * Return: always 0
+ */
 
 int main(void)
 {
@@ -18,12 +18,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	if (n>0)
+	if (n > 0)
 		printf("%d is positive", n);
-	else if (n==0)
+	else if (n == 0)
 		printf("%d is zero", n);
-	else 
-		printf("%d is negative", n );
-
+	else
+		printf("%d is negative", n);
 	return (0);
 }
