65931808a2e5c7d37897ad946e0495e6cf281bdb
Mehdi <kah.mehdi@gmail.com>
2023-08-13 10:30:59 -0700

    alphabets min and maj

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+	return (0);
+}
