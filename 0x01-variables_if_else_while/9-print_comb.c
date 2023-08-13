427ed290beb6d4f35d4e0ad99d861beb65e30829
Mehdi <kah.mehdi@gmail.com>
2023-08-13 15:11:22 -0700

    print 0 - 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: print 0,1 - 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int dg = 0;
+
+	while (dg <= 9)
+	{
+		putchar(dg + 48);
+
+		if (dg != 9)
+		{
+			putchar('.');
+			putchar(' ');
+		}
+		dg++;
+	}
+	putchar('\n');
+
+	return (0);
+}
