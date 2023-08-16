2c6bf6bd6a3fd375ead23db2462bcec1122aba5c
Mehdi <kah.mehdi@gmail.com>
2023-08-16 13:41:48 -0700

    alphabet min

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * print_alph - use _putchar function to print a to z
+ */
+
+void print_alph(void)
+{
+	int ch;
+
+	for (ch = 'a' ; ch <= z ; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
