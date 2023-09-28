d4e23d196d5534e9ec04c7e547d8bbee4bb73fe0
mehdikh <kah.mehdi@gmail.com>
2023-09-28 15:33:22 -0700

    binary to uint update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,22 +6,21 @@
  *
  * Return: The converted number or 0 if an error occurs.
  */
+
 unsigned int binary_to_uint(const char *b)
 {
+	int i;
+	unsigned int dec_val = 0;
+
 	if (!b)
 		return (0);
 
-	unsigned int dec_val = 0;
-	int i;
-
-	for (i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-
-		dec_val = (dec_val << 1) | (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
 	return (dec_val);
 }
-
